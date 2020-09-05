0a067e80c1d29245c5a4c8d7917def92a3b8b519
Zun <zunsamuel97@gmail.com>
2020-09-05 14:21:54 -0500

    step 112

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -1,19 +1,16 @@
 package guru.springframework.msscjacksonexamples.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @JsonTest
 class BeerDtoTest extends BaseTest {
 
-    private final String BEER_DTO_JSON = "{\"id\":\"cf70dc88-4b86-43ce-b4e7-e0774cf1ecae\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-09-05T13:11:35.767008-05:00\",\"lastUpdatedDate\":\"2020-09-05T13:11:35.767805-05:00\"}\n";
+    private final String BEER_DTO_JSON = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":\"12.99\",\"createdDate\":\"2019-06-03T21:01:53-0400\",\"lastUpdatedDate\":\"2019-06-03T21:01:53.628287-04:00\",\"myLocalDate\":\"20190603\",\"beerId\":\"8ed4c7eb-ef3a-437e-823e-a26497ed7e71\"}\n";
+
 
     @Test
     void testSerializeDto() throws JsonProcessingException {
